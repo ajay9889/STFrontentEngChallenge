ac36e2fa3170a4d881c7350bdce65a0ec19c4a1b
ajaymaurya <ajay@mpndbash.com>
2021-12-16 01:53:12 +0800

    Added screen ui for signin and register account

---
+++ app/src/main/java/com/synpulse/companydata/stfrontentengchallenge/Presentation/Fragments/RegisterFragment.kt
@@ -0,0 +1,12 @@
+package com.synpulse.companydata.stfrontentengchallenge.Presentation.Fragments
+
+import android.os.Bundle
+import android.view.View
+import com.synpulse.companydata.Core.base.BaseFragment
+import com.synpulse.companydata.stfrontentengchallenge.databinding.RegisterBinding
+
+class RegisterFragment : BaseFragment<RegisterBinding>(RegisterBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/synpulse/companydata/stfrontentengchallenge/Presentation/Fragments/SignInFragment.kt
@@ -0,0 +1,12 @@
+package com.synpulse.companydata.stfrontentengchallenge.Presentation.Fragments
+
+import android.os.Bundle
+import android.view.View
+import com.synpulse.companydata.Core.base.BaseFragment
+import com.synpulse.companydata.stfrontentengchallenge.databinding.SigninBinding
+
+class SignInFragment : BaseFragment<SigninBinding>(SigninBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
