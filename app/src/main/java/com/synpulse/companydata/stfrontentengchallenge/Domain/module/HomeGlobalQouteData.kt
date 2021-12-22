0f0e8915858f0ab5b6305bb82b35b3962098a3c3
ajaymaurya <ajay@mpndbash.com>
2021-12-22 17:34:52 +0800

    Made the row UI

--- app/src/main/java/com/synpulse/companydata/stfrontentengchallenge/Domain/module/HomeGlobalQouteData.kt
+++ app/src/main/java/com/synpulse/companydata/stfrontentengchallenge/Domain/module/HomeGlobalQouteData.kt
@@ -13,32 +13,3 @@ enum class SectionType{
     TITLE,
     ROW
 }
-//
-//private fun setupRecyclerView() = with(viewbinding) {
-//    adapter = DashboardAdapter(this@DashboardFragment)
-//
-//    var items = ArrayList<Any>()
-//    var dashboardTopSubItems = ArrayList<CompanyListItemData>()
-//    dashboardTopSubItems.add(CompanyListItemData("AMZN", "Amazon.com Inc", "+15"))
-//    dashboardTopSubItems.add(CompanyListItemData("AAPL", "Apple Inc", "+15"))
-//    dashboardTopSubItems.add(CompanyListItemData("XOM", "Exxon Mobil Corp", "+15"))
-//    dashboardTopSubItems.add(CompanyListItemData("MSFT", "Microsoft Company", "+15"))
-//    dashboardTopSubItems.add(CompanyListItemData("IBM", "International Business Machines Corporation", "-15"))
-//
-//    var dashboardTopItem = DashboardTopItem("Gainer and Losser", dashboardTopSubItems)
-//    items.add(dashboardTopItem)
-//
-//    items.add("Your WatchLlst")
-//
-//    for(i in 0..9){
-//        items.add(CompanyListItemData("MSFT", "Microsoft Company", "+15"))
-//        items.add(CompanyListItemData("IBM", "International Business Machines Corporation", "-15"))
-//        items.add(CompanyListItemData("AMZN", "Amazon.com Inc", "+15"))
-//    }
-//
-//    adapter.setItems(items)
-//
-//    recyclerviewDashboardHome.layoutManager = LinearLayoutManager(requireContext())
-//    recyclerviewDashboardHome.adapter = adapter
-//
-//}
