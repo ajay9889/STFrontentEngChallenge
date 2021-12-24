273f41970332ee375a98f6c46de7f3ea9371a3d6
ajaymaurya <ajay@mpndbash.com>
2021-12-24 23:04:20 +0800

    Fixed the issue and implemented Search Algolia Api for smart search

---
+++ app/src/main/java/com/synpulse/companydata/stfrontentengchallenge/Core/base/SingleFragmentActivity.kt
@@ -0,0 +1,50 @@
+package com.synpulse.companydata.stfrontentengchallenge.Core.base
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.synpulse.companydata.Core.base.BaseAppActivity
+import com.synpulse.companydata.stfrontentengchallenge.Presentation.Fragments.DashBoardHomeFragment
+import com.synpulse.companydata.stfrontentengchallenge.Presentation.Fragments.SearchFragment
+import com.synpulse.companydata.stfrontentengchallenge.R
+import com.synpulse.companydata.stfrontentengchallenge.databinding.ActivitySingleFragmentBinding
+
+class SingleFragmentActivity:  BaseAppActivity<ActivitySingleFragmentBinding>(ActivitySingleFragmentBinding::inflate){
+    companion object{
+        private const val EX_FRAGMENT_NAME = "ex_fragment_name"
+        private const val EX_BUNDLE = "ex_bundle"
+        fun launchFragment(mContext: Context , fragmentBundle: Pair<String, Bundle>){
+            (Class.forName(fragmentBundle.first).newInstance() as? Fragment) ?.let {
+                val mIntent = Intent(mContext, SingleFragmentActivity::class.java);
+                mContext.startActivity(
+                    mIntent.putExtra(EX_FRAGMENT_NAME, fragmentBundle.first)
+                        .putExtra(EX_BUNDLE, fragmentBundle.second)
+                )
+            }
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                val mFragment= Class.forName(intent.getStringExtra(EX_FRAGMENT_NAME)).newInstance()
+                if(mFragment is SearchFragment)
+                add<SearchFragment>(R.id.fragment_container_view,
+                    args = intent.getBundleExtra(EX_BUNDLE))
+                if(mFragment is DashBoardHomeFragment)
+                    add<DashBoardHomeFragment>(R.id.fragment_container_view,
+                        args = intent.getBundleExtra(EX_BUNDLE))
+            }
+            setupToolbar(viewBinding.toolbar,null,"")
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        for (fragment in supportFragmentManager.fragments) {
+            fragment.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
